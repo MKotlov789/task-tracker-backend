8135f5ffcc998cefa2ab482225a2672540797960
MKotlov789 <mikhailits161@yandex.ru>
2023-09-12 22:21:53 +0300

    Add git actions support

--- src/main/java/ru/mkotlov789/edu/pet/tasktrackerbackend/repository/UserRepository.java
+++ src/main/java/ru/mkotlov789/edu/pet/tasktrackerbackend/repository/UserRepository.java
@@ -1,13 +1,17 @@
 package ru.mkotlov789.edu.pet.tasktrackerbackend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.mkotlov789.edu.pet.tasktrackerbackend.model.User;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+
     Optional<User> findByUsername(String username);
     boolean existsUserByUsername(String username);
 }
--- src/main/java/ru/mkotlov789/edu/pet/tasktrackerbackend/service/UserDetailsService.java
+++ src/main/java/ru/mkotlov789/edu/pet/tasktrackerbackend/service/UserDetailsService.java
@@ -1,9 +1,11 @@
 package ru.mkotlov789.edu.pet.tasktrackerbackend.service;
 
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import ru.mkotlov789.edu.pet.tasktrackerbackend.model.User;
 import ru.mkotlov789.edu.pet.tasktrackerbackend.repository.UserRepository;
 
 
@@ -16,5 +18,9 @@ public class UserDetailsService implements org.springframework.security.core.use
 
         return userRepository.findByUsername(username)
                 .orElseThrow(()-> new UsernameNotFoundException("user "+username+ " is not found"));
+    }
+    public void deleteUserByUsername(String username) throws UsernameNotFoundException {
+        User user = (User) loadUserByUsername( username);
+        userRepository.delete(user);
     }
 }
